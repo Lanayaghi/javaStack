d7e18adea6b19e07435b2de0acd90417d9857112
lana yaghi <yaghilana.alaa@gmail.com>
2021-06-14 14:57:46 +0300

    comparable interface

--- spring/Author/src/Author/Author.java
+++ spring/Author/src/Author/Author.java
@@ -1,5 +1,30 @@
 package Author;
 
-public class Author {
+public class Author implements Comparable<Author> {
+	protected  String first_name;
+	protected String last_name;
+	protected String book_name;
+	
+public Author(String first_name, String last_name, String book_name){
+	this.first_name = first_name;
+	this.last_name = last_name;
+	this.book_name = book_name;
+}
+
+@Override
+public int compareTo(Author o) {
+	if(this.first_name== o.first_name)  
+		return 0;  
+   else if( first_name.compareTo (o.first_name)< 0) 
+		return -1;
+   else  
+		return 1; 
+	
+}
+
+
+
+
+
 
 }
---
+++ spring/Author/src/Author/Test.java
@@ -0,0 +1,11 @@
+package Author;
+
+public class Test { 
+	public static void main(String[] args) {
+		Author ibtsal = new Author("ibtsal", "awashereh", "java");
+		Author ibtsal1 = new Author("noor", "awashereh", "java");
+		System.out.println(ibtsal.compareTo(ibtsal1));
+		
+}
+
+}
