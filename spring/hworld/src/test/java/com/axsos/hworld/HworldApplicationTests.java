ffe95b7caae17273c4c5c9538e950e53828f531a
lana yaghi <yaghilana.alaa@gmail.com>
2021-06-15 14:45:36 +0300

    new assignment

---
+++ spring/hworld/src/test/java/com/axsos/hworld/HworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axsos.hworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
