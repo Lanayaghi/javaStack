ad934addaf629c9e077ba810bb0b9bf518e9f9a8
lana yaghi <yaghilana.alaa@gmail.com>
2021-06-17 11:56:23 +0300

    counter(practice)

---
+++ spring/Counter/src/main/java/com/axsos/Counter/CounterController.java
@@ -0,0 +1,40 @@
+package com.axsos.Counter;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/you_server")
+public class CounterController {
+	
+	@RequestMapping("")
+	public String server(HttpSession session) {
+		if (session.isNew()) {
+			session.setAttribute("count", 0);
+		}
+		else {
+			Integer count = (Integer) session.getAttribute("count");
+			count++;
+			session.setAttribute("count", count);
+			
+		}
+			
+		return "server.jsp";
+		
+		  
+	}
+	@RequestMapping("/counter")
+	public String counter(HttpSession session) {
+		
+		return"counter.jsp";
+	}
+	
+	@RequestMapping("/reset")
+	public String reset(HttpSession session) {
+		session.setAttribute("count", 0);
+		return"server.jsp";
+	}
+   
+}
